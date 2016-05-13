d7fb7b960334cfcbb9ca6d2671eff3b1029df814
Mark <mark@mopedoversum.de>
2016-05-13 13:41:06 +0200

    Initial structure of the plugin
    added gitignore and initial README as well

---
+++ JavadocExtender/src/javadocextender/JavadocCompletionProposalComputer.java
@@ -0,0 +1,49 @@
+package javadocextender;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.ui.text.java.ContentAssistInvocationContext;
+import org.eclipse.jdt.ui.text.java.IJavaCompletionProposalComputer;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.contentassist.IContextInformation;
+
+public class JavadocCompletionProposalComputer implements IJavaCompletionProposalComputer {
+
+	public JavadocCompletionProposalComputer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void sessionStarted() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ICompletionProposal> computeCompletionProposals(ContentAssistInvocationContext context,
+			IProgressMonitor monitor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<IContextInformation> computeContextInformation(ContentAssistInvocationContext context,
+			IProgressMonitor monitor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getErrorMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void sessionEnded() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # JavadocExtender
+
+This repository contains eclipse plugins.
+
+This project aims to extend the functionality that javadoc
+offers inside eclipse. The user shall be able to add new
+tags (like these "@return" elements) and define valid values
+behind this tag.
+
+Features:
+- autocompletion of tags and valid values
+- Upon compile, check whether the given element is valid and if not, report it
+- Navigation to the definition of the referenced elements
+- Extension Point at which providers can declare the new tags, elements and (optionally) navigation targets
